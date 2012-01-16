6538c0ee1a88098f97be86f06a87fcc6623b3a48
Geoffrey Mainland <gmainlan@microsoft.com>
2012-01-16 14:43:07 +0000

    Add a few more test cases.

--- examples/prim/Main.hs
+++ examples/prim/Main.hs
@@ -17,7 +17,9 @@ main = do
     print $ FX4# (bar 0.0 v# 0.0)
     let g = bar 0.0
     print $ FX4# (g v# 0.0)
-    print $ FX4# (app (\x -> x) v#)
+    print $ FX4# (app (\x -> baz x 10.0) v#)
+    print $ FX4# (baz v# 10)
+    print $ FX4# (zop v# 3)
   where
     !v# = packFloatX4# 1.0# 2.0# 3.0# 4.0#
 
@@ -30,6 +32,16 @@ bar :: Float -> FloatX4# -> Double -> FloatX4#
 bar _ x _ =
     case x of
       _ -> x
+
+baz :: FloatX4# -> Float -> FloatX4#
+{-# NOINLINE baz #-}
+baz v (F# f) =
+    insertFloatX4# v f 1#
+
+zop :: FloatX4# -> Int -> FloatX4#
+{-# NOINLINE zop #-}
+zop v (I# i) =
+    insertFloatX4# v 10.0# i
 
 app :: (FloatX4# -> FloatX4#) -> FloatX4# -> FloatX4#
 {-# NOINLINE app #-}
