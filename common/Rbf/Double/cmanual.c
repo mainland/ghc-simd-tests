b515af17a6e2091ae0257fcaf8ff65ef29c6eda2
Geoffrey Mainland <mainland@apeiron.net>
2013-03-14 17:05:34 +0000

    Use dnrm2 in the BLAS rbf implementation.

--- common/Rbf/Double/cmanual.c
+++ common/Rbf/Double/cmanual.c
@@ -9,7 +9,12 @@ double crbf(double nu, double* u, int ul, double* v, int vl)
 {
     int n = ul < vl ? ul : vl;
     double temp;
+    double nrm2u;
+    double nrm2v;
 
-    temp = cblas_ddot(n, u, 1, u, 1) -2*cblas_ddot(n, u, 1, v, 1) + cblas_ddot(n, v, 1, v, 1);
+    nrm2u = cblas_dnrm2(n, u, 1);
+    nrm2v = cblas_dnrm2(n, v, 1);
+
+    temp = nrm2u*nrm2u -2*cblas_ddot(n, u, 1, v, 1) + nrm2v*nrm2v;
     return exp(-nu*temp);
 }
