3c820bba4811c52c4a917020e39499a62972a164
Geoffrey Mainland <mainland@apeiron.net>
2013-03-14 17:08:31 +0000

    Make random vector generation more imperative :)

--- common/Util/Random.hs
+++ common/Util/Random.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- |
 -- Module      : Util.Random
 -- Copyright   : (c) Geoffrey Mainland 2011-2012
@@ -15,6 +17,7 @@ module Util.Random (
 import Control.Applicative
 import Control.Exception (evaluate)
 import Control.Monad (replicateM, zipWithM_)
+import Control.Monad.Primitive (PrimState)
 import Data.Primitive.Addr
 import Data.Primitive.ByteArray
 import Data.Primitive (sizeOf)
@@ -22,22 +25,37 @@ import Data.Time.Clock.POSIX (getPOSIXTime)
 import qualified Data.Vector.Storable as S
 import qualified Data.Vector.Storable.Mutable as MS
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as MU
 import Foreign.C
 import Foreign.Ptr
 import Foreign.Storable
-import System.Random (Random, newStdGen, randomRs)
+import System.Random (Random, RandomGen, newStdGen, randomR)
 import Text.Printf
 
 import Util.MmMalloc
 
-randomMM :: (Storable a,Random a) => Int -> (a, a) -> IO (S.Vector a)
+randomMM :: forall a . (Storable a,Random a) => Int -> (a, a) -> IO (S.Vector a)
 randomMM n range = do
     gen  <- newStdGen
-    mvec <- MS.unsafeFromForeignPtr0 <$> mmMallocForeignPtr n 16 <*> return n
-    zipWithM_ (\i a -> MS.write mvec i a) [0..] (take n (randomRs range gen))
-    S.freeze mvec
+    mvec <- MS.unsafeFromForeignPtr0 <$> mmMallocForeignPtr n 32 <*> return n
+    fill mvec gen n 0
+  where
+    fill :: RandomGen g => S.MVector (PrimState IO) a -> g -> Int -> Int -> IO (S.Vector a)
+    fill mvec _ n i | i == n = S.freeze mvec
+    fill mvec g n i = do
+        let (x, g') = randomR range g
+        MS.write mvec i x
+        fill mvec g' n (i+1)
 
-randomU :: (U.Unbox a,Random a) => Int -> (a, a) -> IO (U.Vector a)
+randomU :: forall a . (U.Unbox a,Random a) => Int -> (a, a) -> IO (U.Vector a)
 randomU n range = do
     gen <- newStdGen
-    return $ U.fromList $ take n (randomRs range gen)
+    mvec <- MU.new n
+    fill mvec gen n 0
+  where
+    fill :: RandomGen g => U.MVector (PrimState IO) a -> g -> Int -> Int -> IO (U.Vector a)
+    fill mvec _ n i | i == n = U.freeze mvec
+    fill mvec g n i = do
+        let (x, g') = randomR range g
+        MU.write mvec i x
+        fill mvec g' n (i+1)
